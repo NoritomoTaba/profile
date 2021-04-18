324aeecfe7d1fd7cde07f529bbde4ab3b48389e2
cat2koban <cat2koban@users.noreply.github.com>
2021-04-18 20:26:41 +0900

    style: ignore paginate logic

--- components/ArticleList.vue
+++ components/ArticleList.vue
@@ -1,11 +1,11 @@
 <template>
   <section>
-    <h3 class="font-bold text-3xl lg:text-4xl text-gray-800 px-3">
-      Latest posts
+    <h3 class="font-bold text-3xl lg:text-4xl text-gray-700 px-3">
+      Posts
     </h3>
     <article
       v-for="(post,key) in bloglist" :key="key"
-      class="py-2 px-3 rounded-lg hover:bg-gray-200 hover:shadow-inner"
+      class="py-2 px-3 rounded-lg"
       :class="key == bloglist.length -1 ? '' : 'mb-1'"
     >
       <nuxt-link :to="`/blog/${post.slug}`">
@@ -19,19 +19,13 @@
           </figure>
       </div>
       <div>
-        <h4 class="text-xl font-normal text-purple-600">
+        <h4 class="text-xl font-normal text-purple-600 hover:underline">
           {{ post.title }}
         </h4>
-        <p class="mt-1 text-sm text-gray-700">{{ post.description }}</p>
         <p class="mt-1 text-sm text-gray-600">Published on {{ post.ctime }}</p>
       </div>
       </nuxt-link>
     </article>
-    <div
-      v-if="$store.state.bloglist.length > postsPerPage && !isPaginated"
-      class="px-3"
-    >
-    </div>
   </section>
 </template>
 
@@ -39,19 +33,11 @@
 export default {
   computed: {
     bloglist() {
-      if ( ! this.isPaginated ) {
-        return this.$store.state.bloglist.slice(0,this.postsPerPage);
-      } else {
-        return this.$store.state.bloglist;
-      }
+      return this.$store.state.bloglist;
     },
-    totalPages() {
-      return this.isPaginated ? Math.ceil(this.$store.state.bloglist.length / this.postsPerPage) : 1
-    }
   },
   props: {
     isPaginated: Boolean,
-    postsPerPage: Number
   }
 };
 </script>
