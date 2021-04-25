06c2dd3afeb6e1e9159087d28f0740f6873da8b3
cat2koban <cat2koban@users.noreply.github.com>
2021-04-25 20:38:01 +0900

    fix: indent ;(

--- components/ArticleList.vue
+++ components/ArticleList.vue
@@ -9,21 +9,21 @@
       :class="key == bloglist.length -1 ? '' : 'mb-1'"
     >
       <nuxt-link :to="`/blog/${post.slug}`">
-      <div v-if="post.cover_image">
-          <figure>
-            <img
-              :src="require(`~/assets/images/articles/${post.cover_image}?size=640`)"
-              :alt="post.cover_image_cp"
-              loading="lazy"
-            >
-          </figure>
-      </div>
-      <div>
-        <h4 class="font-mono text-xl hover:text-blue-500 underline">
-          {{ post.title }}
-        </h4>
-        <p class="mt-1 text-sm text-gray-600">Published on {{ post.ctime }}</p>
-      </div>
+        <div v-if="post.cover_image">
+            <figure>
+              <img
+                :src="require(`~/assets/images/articles/${post.cover_image}?size=640`)"
+                :alt="post.cover_image_cp"
+                loading="lazy"
+              >
+            </figure>
+        </div>
+        <div>
+          <h4 class="font-mono text-xl hover:text-blue-500 underline">
+            {{ post.title }}
+          </h4>
+          <p class="mt-1 text-sm text-gray-600">Published on {{ post.ctime }}</p>
+        </div>
       </nuxt-link>
     </article>
   </section>
